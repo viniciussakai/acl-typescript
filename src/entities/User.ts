ef54745ba97f28f39ec7bc07b55ec9ec08466bf5
Vinicius Sakai <viniciussakai0@gmail.com>
2021-12-14 14:08:48 -0300

    refact(entities): Updated User entity with relationships

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,7 @@
-import { Column, Entity } from 'typeorm'
+import { Column, Entity, JoinTable, ManyToMany } from 'typeorm'
 import { BaseEntity } from './BaseEntity'
+import { Permission } from './Permission'
+import { Role } from './Role'
 
 @Entity('users')
 export class User extends BaseEntity {
@@ -8,4 +10,20 @@ export class User extends BaseEntity {
 
 	@Column()
 	password: string
+
+	@ManyToMany(() => Role)
+	@JoinTable({
+		name: 'users_roles',
+		joinColumns: [{ name: 'user_id' }],
+		inverseJoinColumns: [{ name: 'role_id' }]
+	})
+	roles: Role[]
+
+	@ManyToMany(() => Permission)
+	@JoinTable({
+		name: 'users_permissions',
+		joinColumns: [{ name: 'user_id' }],
+		inverseJoinColumns: [{ name: 'permission_id' }]
+	})
+	permissions: Permission[]
 }
